5c39d04f638f38b7e027266701141fde96ee7757
Peders123 <rory@caston.co.uk>
2022-02-21 16:22:37 +0000

    fixed linter issues

--- campus_game_project/home/views.py
+++ campus_game_project/home/views.py
@@ -1,14 +1,17 @@
-#from django.shortcuts import render
+# from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
 def home(request):
     print(request)
     return HttpResponse("<h1>HOME</h1>")
 
+
 def leaderboard(request):
     print(request)
     return HttpResponse("<h1>LEADERBOARD</h1>")
+
 
 def shop(request):
     print(request)
