a8ff4d10f933de1b4d78a89429cb766f9de66cf7
Piasttorun <91672079+Piasttorun@users.noreply.github.com>
2022-03-02 18:34:52 +0000

    Update 0003_game_players_finished.py

--- campus_game_project/game/migrations/0003_game_players_finished.py
+++ campus_game_project/game/migrations/0003_game_players_finished.py
@@ -5,10 +5,12 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
+    
     dependencies = [
         ('game', '0002_game_winner'),
     ]
 
+    # Updates game model after end state reached
     operations = [
         migrations.AddField(
             model_name='game',
