20f9c98325c61ecbf4892e2f40df26d2e3763a60
Rory Caston <rory@caston.co.uk>
2022-03-02 22:00:57 +0000

    fixed python linter

--- campus_game_project/welcome/urls.py
+++ campus_game_project/welcome/urls.py
@@ -5,7 +5,4 @@ from . import views
 app_name = 'welcome'
 urlpatterns = [
     path('', views.welcome, name='welcome'),
-    #path('signin', views.sign_in, name='signin'),
-    #path('signout', views.sign_out, name='signout'),
-    #path('callback', views.callback, name='callback'),
 ]
--- campus_game_project/welcome/views.py
+++ campus_game_project/welcome/views.py
@@ -1,55 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
-from django.urls import reverse
-from datetime import datetime, timedelta
-from dateutil import tz, parser
-from welcome.auth_helper import get_sign_in_flow, get_token_from_code, store_user, remove_user_and_token, get_token
-from welcome.graph_helper import *
+
 
 def welcome(request):
     return render(request, 'welcome/welcome.html')
-
-'''def initialize_context(request):
-  context = {}
-
-  # Check for any errors in the session
-  error = request.session.pop('flash_error', None)
-
-  if error != None:
-    context['errors'] = []
-    context['errors'].append(error)
-
-  # Check for user in the session
-  context['user'] = request.session.get('user', {'is_authenticated': False})
-  return context
-
-def sign_in(request):
-  # Get the sign-in flow
-  flow = get_sign_in_flow()
-  # Save the expected flow so we can use it in the callback
-  try:
-    request.session['auth_flow'] = flow
-  except Exception as e:
-    print(e)
-  # Redirect to the Azure sign-in page
-  return HttpResponseRedirect(flow['auth_uri'])
-
-def sign_out(request):
-  # Clear out the user and token
-  remove_user_and_token(request)
-
-  return HttpResponseRedirect(reverse('home'))
-
-
-def callback(request):
-  # Make the token request
-  result = get_token_from_code(request)
-
-  #Get the user's profile
-  user = get_user(result['access_token'])
-
-  # Store user
-  store_user(request, user)
-  return HttpResponseRedirect(reverse('welcome'))'''
-
-
