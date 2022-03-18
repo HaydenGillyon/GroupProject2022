21b35eb17a0fe61ffe8239dfd661ac6fb3b15f86
Rory Caston <rory@caston.co.uk>
2022-03-18 01:35:00 +0000

    python linter

--- campus_game_project/welcome/models.py
+++ campus_game_project/welcome/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class User(models.Model):
 
@@ -7,7 +8,7 @@ class User(models.Model):
     email = models.CharField(max_length=25)
     password = models.CharField(max_length=100)
     status = models.IntegerField(default=1)
-    
+
 
 class Admin(models.Model):
 
