f85d861b5e6bd7f7a2cf6fb419c3866d9778c80b
Zagox11 <72284724+HaydenGillyon@users.noreply.github.com>
2022-03-23 18:15:56 +0000

    Register models with Django admin page and add wss support to lobby and running + fix lobby radius

--- campus_game_project/welcome/admin.py
+++ campus_game_project/welcome/admin.py
@@ -1,3 +1,6 @@
-# from django.contrib import admin
+from django.contrib import admin
+from welcome.models import User, Admin
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Admin)
